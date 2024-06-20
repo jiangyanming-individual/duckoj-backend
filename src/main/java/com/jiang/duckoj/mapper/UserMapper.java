869f79d1a999b29fdc6ae698cc06219663616433
jiangyanming <1635156042@qq.com>
2024-06-20 21:22:19 +0800

    Initial commit

---
+++ src/main/java/com/jiang/duckoj/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.jiang.duckoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jiang.duckoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
